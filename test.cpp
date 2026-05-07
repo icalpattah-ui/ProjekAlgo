29af4486284ebaf3c5b5d07cf2a42a09f621f8a7
galleanmvd <124250039@student.upnyk.ac.id>
2026-05-07 11:30:52 +0700

    menambahkan void simpanFile untuk menyimpan, menambah, dan mengubah file di lokal

--- test.cpp
+++ test.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 using namespace std;
 
+film *head = NULL;
 struct film
 {
-    string judul;
-    string  genre;
-    string tanggal;
+    char judul;
+    char  genre;
+    char tanggal;
     int harga;
     int durasi;
     film* next;
 };
 
+void simpanFile(){
+    FILE* f = fopen("film.txt", "w");
+    if (f = NULL){
+        cout << "File tidak bisa dibuka" << endl;
+        return;
+    }
+
+    film *temp = head;
+
+    while(temp != NULL){
+        fprintf(f, "%s|%s|%s|%d|%d",
+            temp->judul,
+            temp->genre,
+            temp->tanggal,
+            temp->harga,
+            temp->durasi);
+        temp = temp->next;
+    }
+    fclose(f);
+}
+
+
+void loadFile(){
+    FILE* f = fopen("");
+}
