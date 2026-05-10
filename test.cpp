3c8168291900af8d01defb5bce6fa686c3981c3a
galleanmvd <124250039@student.upnyk.ac.id>
2026-05-10 12:45:21 +0700

    menambahkan fungsi loadFile

--- test.cpp
+++ test.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 using namespace std;
 
-film *head = NULL;
 struct film
 {
-    char judul;
-    char  genre;
-    char tanggal;
+    char judul[100], genre[100], tanggal[100];
     int harga;
     int durasi;
     film* next;
 };
 
+film *head = NULL;
+
 void simpanFile(){
-    FILE* f = fopen("film.txt", "w");
-    if (f = NULL){
+    FILE *f = fopen("film.txt", "w");
+    if (f == NULL){
         cout << "File tidak bisa dibuka" << endl;
         return;
     }
 
     film *temp = head;
 
-    while(temp != NULL){
+    while (temp != NULL){
         fprintf(f, "%s|%s|%s|%d|%d",
             temp->judul,
             temp->genre,
@@ -31,9 +30,36 @@ void simpanFile(){
         temp = temp->next;
     }
     fclose(f);
+    cout << "Data berhasil disimpan ke file" << endl;
 }
 
 
 void loadFile(){
-    FILE* f = fopen("");
+    FILE *f = fopen("film.txt", "r");
+    
+    if(f == NULL) return;
+    film *tail = NULL; // buat append & jaga urutan asli
+    film *filmBaru = new film;
+
+    while (fscanf(f, "%[^|]|%[^|]|%[^|]|%d|%d",
+                    filmBaru->judul,
+                    filmBaru->genre,
+                    filmBaru->tanggal,
+                    &filmBaru->harga,
+                    &filmBaru->durasi) == 6 )
+    {
+        //
+        filmBaru->next = NULL;
+        if(head == NULL){
+            head = filmBaru;
+            tail = filmBaru;
+        } else {
+            tail->next = filmBaru;
+            tail = filmBaru;
+        }
+
+        filmBaru = new film;
+    }
+    delete filmBaru; //
+    fclose(f);
 }
